c9539f8fac92f6368a8b277f640baeb6fee5f402
Young <37269766+Youngpig1998@users.noreply.github.com>
2021-04-23 22:03:02 +0800

    Update example.rb
    增加了字段判空

--- fluentd/example.rb
+++ fluentd/example.rb
@@ -1,12 +1,24 @@
 def filter(tag, time, record)
-  if(File.exist?('/var/log/env'))
-    env=Hash[*File.read('/var/log/env').split(/[= \n]+/)]
-  else
-    env={}
-  end
 
+  
+  
+  # if(File.exist?('/var/log/env'))
+  #   env=Hash[*File.read('/var/log/env').split(/[= \n]+/)]
+  # else
+  #   env={}
+  # end
+
+  
+
+  #如果logSourceCRN和message的value为空字符串
   $logSourceCRN = record["logSourceCRN"]
+  if $logSourceCRN == ""
+    $logSourceCRN = "crn:v1:production:us-south:assistant:icp:a/02a92df0-657c-43c9-94fc-2280450b1e0b:00000000-0000-0000-1617-768394230058::"
+  end
   $message = record["message"]
+  if $message == ""
+    $message = "Watson Assistant: delete skill"
+  end
 
   record = record.merge({:"attachments" => [{:"name" => "ibm-cp-data"}]})
   record[:"attachments"][0] = record[:"attachments"][0].merge({:"contentType" => "http://schemas.ibm.com/cloud/content/1.0/cloudpak"})
@@ -14,10 +26,53 @@ def filter(tag, time, record)
   record[:"attachments"][0] = record[:"attachments"][0].merge({:"message" => $message})
   record[:"attachments"][0] = record[:"attachments"][0].merge({:"content" => {:"sourceCrn" => 10}})
 
-  record[:"attachments"][0][:"content"] = record[:"attachments"][0][:"content"].merge({:"kubernetes" => {:"container_id" => env["CONTAINERID"]}})
-  record[:"attachments"][0][:"content"][:"kubernetes"] = record[:"attachments"][0][:"content"][:"kubernetes"].merge({:"container_name" => env["CONTAINERNAME"]})
-  record[:"attachments"][0][:"content"][:"kubernetes"] = record[:"attachments"][0][:"content"][:"kubernetes"].merge({:"namespace" => env["NAMESPACE"]})
-  record[:"attachments"][0][:"content"][:"kubernetes"] = record[:"attachments"][0][:"content"][:"kubernetes"].merge({:"pod" => env["PODIPADDRESS"]})
+
+  if(File.exist?('/var/log/env'))
+    f = File.open('/var/log/env','r')
+    context = f.readlines
+    n = context[0].split(/[ \n]+/)
+    containerName = context[1].split(/[ \n]+/)
+    nodeName = context[2].split(/[ \n]+/)
+    containerId = context[3].split(/[ \n]+/)
+    podIpAdd = context[4].split(/[ \n]+/)
+
+
+    if n.join("") == "NAMESPACE="
+      n = "NOT_SPECIFIED"
+    else
+      n = n[0].split(/NAMESPACE=/).join("")
+    end
+    if containerName.join("") == "CONTAINERNAME="
+      containerName = "NOT_SPECIFIED"
+    else
+      containerName = containerName[0].split(/CONTAINERNAME=/).join("")
+    end
+    if nodeName == "NODENAME="
+      nodeName = "NOT_SPECIFIED"
+    else
+      nodeName = nodeName[0].split(/NODENAME=/).join("")
+    end
+    if containerId == "CONTAINERID="
+      containerId = "NOT_SPECIFIED"
+    else
+      containerId = containerId[0].split(/CONTAINERID=/).join("")
+    end
+    if podIpAdd == "PODIPADDRESS="
+      podIpAdd = "NOT_SPECIFIED"
+    else
+      podIpAdd = podIpAdd[0].split(/PODIPADDRESS=/).join("")
+    end
+
+    # str = "#{n}#{s}#{u}#{p}#{q}"
+    # File.open('/var/log/result.txt', 'w') { |file| file.print(str) }
+
+  end
+
+
+  record[:"attachments"][0][:"content"] = record[:"attachments"][0][:"content"].merge({:"kubernetes" => {:"container_id" => containerId}})
+  record[:"attachments"][0][:"content"][:"kubernetes"] = record[:"attachments"][0][:"content"][:"kubernetes"].merge({:"container_name" => containerName})
+  record[:"attachments"][0][:"content"][:"kubernetes"] = record[:"attachments"][0][:"content"][:"kubernetes"].merge({:"namespace" => n})
+  record[:"attachments"][0][:"content"][:"kubernetes"] = record[:"attachments"][0][:"content"][:"kubernetes"].merge({:"pod" => podIpAdd})
   record
 
 
